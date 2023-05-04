29d638856f34beec3a84816c6689e4c5f43742b9
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2023-05-04 19:38:36 +0200

    use setuptools_scm to sync version from git

--- textract2page/__init__.py
+++ textract2page/__init__.py
@@ -1,5 +1,7 @@
 """convert OCR results from Amazon AWS Textract (JSON) to PRImA PAGE (XML)"""
 
 from .convert_aws import convert_file
+from ._version import version
 
 __all__ = ['convert_file']
+__version__ = version
