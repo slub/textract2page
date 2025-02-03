4c349ad13231d93b7be84c1d000ab6fcf79295f6
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2025-02-03 07:55:57 +0100

    add missing import

--- textract2page/__init__.py
+++ textract2page/__init__.py
@@ -1,6 +1,6 @@
 """convert OCR results from Amazon AWS Textract (JSON) to PRImA PAGE (XML)"""
 
-from .convert_aws import convert_file
+from .convert_aws import convert_file, convert_file_without_image
 from ._version import version
 
 __all__ = ['convert_file', 'convert_file_without_image']
